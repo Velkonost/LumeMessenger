f82492447143b8b37b6a913470059f37df334ca9
fastvepik <andrey.yurin55@gmail.com>
2017-10-20 18:16:33 +0300

    fixed login VK

--- app/src/main/java/com/velkonost/lume/MainActivity.java
+++ app/src/main/java/com/velkonost/lume/MainActivity.java
@@ -7,22 +7,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private String[] scope = new String[]{VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(VKSdk.isLoggedIn()) {
+            startActivity(new Intent(MainActivity.this, com.velkonost.lume.vkontakte.activities.MainActivity.class));
+            finish();
+        }
+
         Button btn = (Button) findViewById(R.id.vk);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, com.velkonost.lume.vkontakte.activities.MainActivity.class);
-                MainActivity.this.startActivity(intent);
-
+                //Intent intent = new Intent(MainActivity.this, com.velkonost.lume.vkontakte.activities.MainActivity.class);
+                //MainActivity.this.startActivity(intent);
+                VKSdk.login(MainActivity.this, scope);
             }
         });
 
@@ -34,5 +47,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+                //Toast.makeText(getApplicationContext(), "Good", Toast.LENGTH_LONG).show();
+                // startActivity(new Intent(MainActivity.this, AllAct.class));
+                //finish();
+                startActivity(new Intent(MainActivity.this, com.velkonost.lume.vkontakte.activities.MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+                //Toast.makeText(getApplicationContext(), "Bad", Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
